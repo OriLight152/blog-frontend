08785b984e74ab7c029947525fb4bb5e7dbedc88
OriLight152 <OriLight152@users.noreply.github.com>
2023-02-27 07:27:44 +0000

    deploy: 9ad3f293c11f8392819dcd8e9941c2a0108cfe7b

--- assets/system-setting-874c99f9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{c as t,j as o,l as s,i as c}from"./index-226c82f5.js";const r={},a=s("p",{class:"mb-4 text-2xl font-bold"},"系统设置",-1);function n(_,l){return c(),t("div",null,[a,o(" 暂无设置项 ")])}const i=e(r,[["render",n]]);export{i as default};
